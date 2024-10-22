15a6dc0e4b2fd00cb848d086d57b573a09b82074
SoMaxB <jmaxbarroso@gmail.com>
2024-10-22 13:45:23 +0200

    Funciones agregadas

--- Roadmap/03 - ESTRUCTURAS DE DATOS/c/SoMaxB.c
+++ Roadmap/03 - ESTRUCTURAS DE DATOS/c/SoMaxB.c
@@ -13,3 +13,56 @@
  *   (o el número de dígitos que quieras)
  * - También se debe proponer una operación de finalización del programa.
  */
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+struct Persona
+{
+		char	nombre[20];
+		int		telefono;
+};
+
+int	main(void)
+{
+		struct Persona persona1;
+		
+		insertarPersona(&persona1, "Pepe", 654729893);
+		return 0;
+}
+
+//Inserción
+void	insertarPersona(struct Persona* p, const char* nombre, int telefono)
+{
+		strcpy(p->nombre, nombre);
+		p->telefono = telefono;
+}
+
+//Borrar una estructura asignando valores por defecto
+void	borrarPersona(struct Persona* p)
+{
+		strcpy(p->nombre, ""); // Vaciar nombre
+		p->telefono = 0;
+}
+
+//Actualizar estructura
+void	actualizarPersona(struct Persona* p, const char* nuevoNombre, int nuevoTelefono)
+{
+		strcpy(p->nombre, nuevoNombre);
+		p->telefono = nuevoTelefono;
+}
+
+//Comparación (usada para luego ordenar)
+int	compararPorNombre(const void* a, const void* b)
+{
+		struct Persona* persona1 = (struct Persona*)a;
+		struct Persona* persona2 = (struct Persona*)b;
+		return strcmp(persona1->nombre, persona2->nombre);
+}
+
+//Ordenación
+void	ordenarPorNombre(struct Persona personas[], int size)
+{
+		qsort(personas, size, sizeof(struct Persona), compararPorNombre);
+}
